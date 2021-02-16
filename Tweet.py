b4c5e8eed4b1ae97e1e4be73f46504b33d5fde02
lcaliu23 <lucia.liu@utp.ac.pa>
2021-02-15 22:18:32 -0500

    Updated cleaning, categorization and classification class

--- Tweet.py
+++ Tweet.py
@@ -21,9 +21,8 @@ def preprocess(tweet):
     # remove multiple spaces
     tweet = re.sub("\s+", " ", tweet)
     tweet = re.sub('^\s+', '', tweet)
-    # lower first character
-    try: tweet = tweet[0].lower() + tweet[1:]
-    except: pass
+    # lower characters
+    tweet = tweet.lower()
     # remove stop words
     tweet = ' '.join([word for word in tweet.split(' ') 
                 if not word.lower() in stop_words])
@@ -32,20 +31,22 @@ def preprocess(tweet):
 def clean_text(dataframe, column_name):
     '''Receives a dataframe with the text column name and returns
         the dataframe with a clean text column.'''
-    dataframe['clean'] = dataframe[column_name]
-    dataframe['clean'] = dataframe.apply(lambda row: preprocess(row.clean), axis=1)
-    return dataframe
+    df = dataframe[:]
+    df['clean'] = df[column_name]
+    df['clean'] = df.apply(lambda row: preprocess(row.clean), axis=1)
+    return df
 
 def classify_text(dataframe, column_name):
     '''Classifies tweet text depending of the keywords contained.
         Returns dataframe with classification data.'''
+    df = dataframe[:]
     # keywords for accident tweets
-    dataframe['isAccident'] = np.where(dataframe[column_name].str.contains('accidente|accidentaron|accidentó|choque|chocó|chocaron|choca|chocar|colisión|colisionaron|colisionó|colisiona|colisionado|vuelco|volcó|vuelca|atropello|atropellado'), 1, 0)
+    df['isAccident'] = np.where(df[column_name].str.contains('accidente|accidentaron|accidentó|choque|chocó|chocaron|choca|chocar|colisión|colisionaron|colisionó|colisiona|colisionado|vuelco|volcó|vuelca|atropello|atropellado'), 1, 0)
     # keywords for obstacles incidents
-    dataframe['isObstacle'] = np.where(dataframe[column_name].str.contains('tranque|trancado|embotellamiento|desplazan autos|vistas tráfico|movimiento vehicular|huelga|motín|protesta|protestando|trabajos vía|trabajos|trabajos ruta|cierre|cerraron|cerrado|cierran|cierra|daño|dañó|detuvieron|detenido|detenida|obstáculo|obstaculizando|parado|paro|paño cerrado|tráfico paralizado|tráfico detenido|tráfico afectado|tráfico pesado|tráfico lento|desvío|área acordonada'), 1, 0)
+    df['isObstacle'] = np.where(df[column_name].str.contains('tranque|trancado|embotellamiento|desplazan autos|vistas tráfico|movimiento vehicular|huelga|motín|protesta|protestando|trabajos vía|trabajos|trabajos ruta|cierre|cerraron|cerrado|cierran|cierra|daño|dañó|detuvieron|detenido|detenida|obstáculo|obstaculizando|parado|paro|paño cerrado|tráfico paralizado|tráfico detenido|tráfico afectado|tráfico pesado|tráfico lento|desvío|área acordonada'), 1, 0)
     # keywords for danger incidents
-    dataframe['isDanger'] = np.where(dataframe[column_name].str.contains('incendio|incendia|incendiando|incendiado|incendiaron|inundado|inundación|inundó'), 1, 0)
+    df['isDanger'] = np.where(df[column_name].str.contains('incendio|incendia|incendiando|incendiado|incendiaron|inundado|inundación|inundó'), 1, 0)
     # finally assigns if it's incident
-    dataframe['isIncident'] = np.where(dataframe[['isAccident','isObstacle','isDanger']].sum(axis=1)>0, 1, 0)
+    df['isIncident'] = np.where(df[['isAccident','isObstacle','isDanger']].sum(axis=1)>0, 1, 0)
     # returns dataframe back
-    return dataframe
+    return df
