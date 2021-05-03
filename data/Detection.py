be83da12b8b839d5dbe4b7a927630289c6d1f98b
lcaliu23 <lucia.liu@utp.ac.pa>
2021-05-02 20:40:02 -0500

    Added docstrings and fix in chained warning

--- data/Detection.py
+++ data/Detection.py
@@ -1,4 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Resume
+
+Created on 
+@author: Lucia Liu (lucia.liu@utp.ac.pa)
+"""
+
+import Preprocessing
 import pickle
+
 # load  vectorizers
 with open('./models/vectorizer3000', 'rb') as vect1:
     vectorizer3 = pickle.load(vect1)
@@ -12,15 +23,26 @@ with open('./models/mo_3000', 'rb') as training_model:
 
 # asdf
 def get_classification(dataframe, column):
-    '''Receives dataframe with text and returns dataframe with
-     classification and categorization data.'''
+    """Receives dataframe with text and returns dataframe with
+    classificacion and categorization data.
+
+    Args:
+        dataframe (Pandas.DataFrame): Table with tweets data.
+        column (string): Name of column with tweet text.
+
+    Returns:
+        Pandas.DataFrame: Returns dataframe with classification
+        and categorization.
+    """
     # copies dataframe
-    df = dataframe.iloc[:]
+    df = dataframe.copy()
+    # cleans data
+    df = Preprocessing.clean_text(df, 'text')
     ## vectorizes data
     vect_data3 = vectorizer3.transform(df[column])
     vect_data10 = vectorizer10.transform(df[column])
     ## classifies and categorizes data
-    df['isIncident'] = xgb_classifier.predict(vect_data10)
-    df[['isAccident','isObstacle','isDanger']] = mo_classifier.predict(vect_data3)
+    df.loc[:,'isIncident'] = xgb_classifier.predict(vect_data10)
+    df.loc[:,['isAccident','isObstacle','isDanger']] = mo_classifier.predict(vect_data3)
     # returns result dataframe
     return df
