b701a1941a40c863860e4a076aadeef7d96a37cf
lcaliu23 <lucia.liu@utp.ac.pa>
2021-05-02 20:37:52 -0500

    Added docstring and fixed chained warning

--- data/Preprocessing.py
+++ data/Preprocessing.py
@@ -1,11 +1,17 @@
 import re, json
 import pandas as pd, numpy as np
 
-stop_words = json.load(open('./others/stop_words.json', encoding='utf8'))
+stop_words = json.load(open('./files/stop_words.json', encoding='utf8'))
 
 def preprocess(tweet):
-    '''Removes next lines, links, mentions, hashtags, stop words, etc.
-    Returns a clean tweet text.'''
+    """Removes next lines, links, mentions, hashtags, stop words, etc.
+
+    Args:
+        tweet (string): Tweet text.
+
+    Returns:
+        string: Tweet text cleaned.
+    """
     # remove next lines
     tweet = re.sub('\n', ' ', tweet)
     # remove links
@@ -36,24 +42,17 @@ def preprocess(tweet):
     return tweet
 
 def clean_text(dataframe, column_name):
-    '''Receives a dataframe with the text column name and returns
-        the dataframe with a clean text column.'''
-    df = dataframe[:]
-    df['clean'] = df[column_name]
-    df['clean'] = df.apply(lambda row: preprocess(row.clean), axis=1)
-    return df
+    """Receives a dataframe with the text column name and returns
+        the dataframe with a clean text column.
 
-def classify_text(dataframe, column_name):
-    '''Classifies tweet text depending of the keywords contained.
-        Returns dataframe with classification data.'''
-    df = dataframe[:]
-    # keywords for accident tweets
-    df['isAccident'] = np.where(df[column_name].str.contains('accidente|accidentaron|accidentó|choque|chocó|chocaron|choca|chocar|colisión|colisionaron|colisionó|colisiona|colisionado|vuelco|volcó|vuelca|atropello|atropellado'), 1, 0)
-    # keywords for obstacles incidents
-    df['isObstacle'] = np.where(df[column_name].str.contains('tranque|trancado|embotellamiento|desplazan autos|vistas tráfico|movimiento vehicular|huelga|motín|protesta|protestando|trabajos vía|trabajos|trabajos ruta|cierre|cerraron|cerrado|cierran|cierra|daño|dañó|detuvieron|detenido|detenida|obstáculo|obstaculizando|parado|paro|paño cerrado|tráfico paralizado|tráfico detenido|tráfico afectado|tráfico pesado|tráfico lento|desvío|área acordonada'), 1, 0)
-    # keywords for danger incidents
-    df['isDanger'] = np.where(df[column_name].str.contains('incendio|incendia|incendiando|incendiado|incendiaron|inundado|inundación|inundó'), 1, 0)
-    # finally assigns if it's incident
-    df['isIncident'] = np.where(df[['isAccident','isObstacle','isDanger']].sum(axis=1)>0, 1, 0)
-    # returns dataframe back
+    Args:
+        dataframe (Pandas.DataFrame): Tweets data table.
+        column_name (string): Tweet text column name.
+
+    Returns:
+        Pandas.DataFrame: Dataframe with a clean text column.
+    """
+    df = dataframe.copy()
+    df.loc[:,'clean'] = df.loc[:,column_name].copy()
+    df.loc[:,'clean'] = df.apply(lambda row: preprocess(row.clean), axis=1)
     return df
