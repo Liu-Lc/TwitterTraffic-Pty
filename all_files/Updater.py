d4dcc60137f2728892afcd0e165f5e90c5ef10c0
lcaliu23 <lucia.liu@utp.ac.pa>
2021-05-09 19:17:23 -0500

    Fixed indentation and added search parameter

--- all_files/Updater.py
+++ all_files/Updater.py
@@ -18,31 +18,32 @@ from Tweet import Tweet
 from keys import *
 
 
-def get_tweets(from_date=None):
-   """Executes the function to get the last 7 days tweets.
+def get_tweets(search=None, from_date=None):
+    """Executes the function to get the last 7 days tweets.
 
-   Args:
-       None.
+    Args:
+        None.
 
-   Returns:
-       Tweet array: Returns an array with all the Tweets found.
-   """
-   # consumer key authentication
-   auth = OAuthHandler(consumer_key, consumer_secret)
-   # access key authentication
-   auth.set_access_token(access_token, access_token_secret)
-   # set up API with authentication handler
-   api = API(auth, wait_on_rate_limit=True)
-   # Define the word to seach and date
-   search_word = "(from:traficocpanama) OR (@traficocpanama) -filter:replies -filter:retweets"
-   if from_date!=None:
-       search_word += " since:" + from_date
-   # Collect tweets
-   tweets = Cursor(api.search,
-                  q=search_word,
-                  # since='2021-04-23',
-                  tweet_mode='extended').items()
-   all_tweets = [Tweet(t.id, t.user.screen_name, t.user.name, t.full_text, t.created_at,
-                  'https://www.twitter.com/' + str(t.user.screen_name) + '/status/' + str(t.id)) 
-               for t in tweets]
-   return all_tweets
+    Returns:
+        Tweet array: Returns an array with all the Tweets found.
+    """
+    # consumer key authentication
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    # access key authentication
+    auth.set_access_token(access_token, access_token_secret)
+    # set up API with authentication handler
+    api = API(auth, wait_on_rate_limit=True)
+    # Define the word to seach and date
+    if search==None:
+        search = "(from:traficocpanama) OR (@traficocpanama) -filter:replies -filter:retweets"
+    if from_date!=None:
+        search += " since:" + from_date
+    # Collect tweets
+    tweets = Cursor(api.search,
+                    q=search,
+                    # since='2021-04-23',
+                    tweet_mode='extended').items()
+    all_tweets = [Tweet(t.id, t.user.screen_name, t.user.name, t.full_text, t.created_at,
+                    'https://www.twitter.com/' + str(t.user.screen_name) + '/status/' + str(t.id)) 
+                for t in tweets]
+    return all_tweets
