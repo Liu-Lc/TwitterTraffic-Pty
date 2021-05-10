578cde71465abfae3db9f95bfe8e514cc8a29ae7
lcaliu23 <lucia.liu@utp.ac.pa>
2021-05-10 16:05:11 -0500

    Renamed all files to TweetData

---
+++ TweetData/keys.py
@@ -0,0 +1,9 @@
+# keys for API
+consumer_key = '2W6H7jverJ52CWSjCILsINxBx'
+consumer_secret = 'X7rVZc6TlnvA6S1HoYdxAWW243S764sA7YV7R1DdMwfKUNLIxU'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAEW5PQEAAAAAGtlNHhM5kl4guLSsHAkUPdDyBpk%3DHFIXlOQZvib6x8tOlRhRxnOUgAPNeoZ2LgxUrk0ggUfvwyVVhM'
+access_token = '1344702616186589185-s6JvacGhVxFcQUdrZtVlIUYenaDU3X'
+access_token_secret = 'uPnX40JjhSMgPJj8uj93yGgm9CzJzP171w0P3XfNhn9Bp'
+
+# pass for db
+db_pass = 'root'
